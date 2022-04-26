b65f2824f542e6facda030c6134f32cb20b3324f
jurgar <102683510+jurgar@users.noreply.github.com>
2022-04-26 20:11:26 +0300

    add comentar

---
+++ 1 paskaita(variables, math  operation)/scriot.js
@@ -0,0 +1,15 @@
+// One  line komentaras
+
+
+/*
+Sveiki cia yra
+Multi line komentaras
+*/
+
+// sukuriam kintamuosius 
+let myName;
+let age;
+
+//priskiriam kintamiesiams reiksmes
+myName = "Jurga";
+age = 46;
