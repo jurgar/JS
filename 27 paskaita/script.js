07d75f909256c9be6f349014bde53aa253ddc700
jurgar <102683510+jurgar@users.noreply.github.com>
2022-06-16 22:44:59 +0300

    exercices , kartojimas

---
+++ 27 paskaita/script.js
@@ -0,0 +1,82 @@
+// const userName = "Jurga";
+// const goodbye = "Viso gero" + " " + userName + " " + "lauksime sugrįžtant !";
+// console.log(goodbye);
+// //geresnis variantas
+
+// const goodbye = `Viso gero ${userName}, lauksime sugrįžtant!`;
+
+// const salary = 1000;
+// const dollarSymbol = "$";
+// console.log(`${dollarSymbol} ${salary}`);
+// console.log(`$${salary}`);
+
+// const price = 10;
+
+// if (price >= 10) {
+//   console.log("Perku");
+// } else {
+//   console.log("Neperku");
+// }
+// arba;
+
+// let result = "";
+// if (price > 10) {
+//   result = "Perku";
+// } else {
+//   result = "Neperku";
+// }
+// console.log(result);
+
+// arba;
+
+// let result1 = "Neperku";
+// if (price > 10) {
+//   result1 = "Perku";
+// }
+// comsole.log(result1);
+
+// const hours = 12;
+
+// if (hours <= 6) {
+//   console.log("naktis");
+// } else if (hours <= 12) {
+//   console.log("rytas");
+// } else if (hours <= 18) {
+//   console.log("diena");
+// } else if (hours <= 24) {
+//   console.log("vakaras");
+// }
+
+// const hours = 0;
+// let result = "";
+
+// if (hours >= 0 && hours < 6) {
+//   result = "Naktis";
+// } else if (hours >= 6 && hours < 12) {
+//   result = "Rytas";
+// } else if (hours >= 12 && hours < 18) {
+//   result = "Diena";
+// } else if (hours >= 18 && hours <= 24) {
+//   result = "Vakaras";
+// } else {
+//   result = "Netinkamas skaicius";
+// }
+
+// console.log(result);
+
+const myMoney = 12;
+const price = myMoney >= 10 ? console.log("Perku") : console.log("Neperku");
+
+const hours = 18;
+const result =
+  hours <= 6
+    ? "Naktis"
+    : hours <= 12
+    ? "Rytas"
+    : hours <= 18
+    ? "Diena"
+    : hours <= 24
+    ? "Vakaras"
+    : "Naktis";
+
+console.log(result);
